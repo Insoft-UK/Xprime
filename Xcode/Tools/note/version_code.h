3a2632ed569344141ed62e5645779cdcb2083aca
insoft <developer@insoft.uk>
2026-02-04 02:02:24 +0000

    Update

--- Xcode/Tools/note/version_code.h
+++ Xcode/Tools/note/version_code.h
@@ -21,9 +21,9 @@
 // SOFTWARE.
 
 #define VERSION_NUMBER        "1.0.0"
-#define VERSION_CODE          "A0A29-26B2"
-#define NUMERIC_BUILD          10029
-#define BUNDLE_VERSION        "20260203"
-#define INTERNAL_BUILD_CODE   "A0A29"
-#define DATE                  "2026 February 03"
+#define VERSION_CODE          "A0A30-26B3"
+#define NUMERIC_BUILD          10030
+#define BUNDLE_VERSION        "20260204"
+#define INTERNAL_BUILD_CODE   "A0A30"
+#define DATE                  "2026 February 04"
 #define YEAR                  "2026"
